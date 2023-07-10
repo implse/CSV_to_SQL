79b93fff6009efc2a21db53be9d1892d91dcee5d
Finite <finite.impulse@gmail.com>
2023-07-10 16:27:27 +0200

    Add Pandas exceptions

--- load_csv_data_to_sqlite.py
+++ load_csv_data_to_sqlite.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+from pandas.errors import ParserError, DatabaseError
 import csv
 import sqlite3
 import argparse
 
+parser = argparse.ArgumentParser(description="CSV to SQL")
+parser.add_argument("-f", help="Path to the .csv file")
+parser.add_argument("-d", help="Path to the .db database")
+parser.add_argument("-t", help="Database table name")
 
-# Pandas: read csv file and write records stored in the dataFrame to a SQL database
+
+# Pandas: read csv file and write records stored in the dataFrame to a SQL database. 
 def csv_to_sqlite(csv, table):
-    file = pd.read_csv(csv, usecols=range(1, 5))
-    file.to_sql(table, conn, if_exists='replace', index=True, index_label='id')
+    try:
+        file = pd.read_csv(csv, usecols=range(1, 5))
+    except ParserError as e:
+        raise e
+    try:
+        file.to_sql(table, conn, if_exists='replace', index=True, index_label='id')
+    except DatabaseError as e:
+        raise e
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="CSV to SQL")
-    parser.add_argument("-f", help="Path to the .csv file")
-    parser.add_argument("-d", help="Path to the .db database")
-    parser.add_argument("-t", help="Database table name")
     args = parser.parse_args()
 
     try:
